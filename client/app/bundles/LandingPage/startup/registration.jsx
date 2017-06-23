28fbcb20964ce38f916fa01c4633afa7398616dc
Sam.Lin <greenlin111@gmail.com>
2017-06-23 10:30:16 +0800

    rename client side hello world to landing page

---
+++ client/app/bundles/LandingPage/startup/registration.jsx
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+import HelloWorld from '../components/Landing';
+
+// This is how react_on_rails can see the Landing Page in the browser.
+ReactOnRails.register({
+  Landing,
+});
